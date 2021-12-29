15ed2098e696bde96b156d72ceafce6adc0e3b2a
SonMinWoo <mwson98@naver.com>
2021-12-30 01:22:33 +0900

    initial commit

---
+++ public/client.js
@@ -0,0 +1,97 @@
+const messageList = document.querySelector(".chattings");
+const nickForm = document.querySelector("#nickname");
+const messageForm = document.querySelector("#message");
+const chatbox = document.querySelector(".chatbox");
+const socket = new WebSocket(`ws://${window.location.host}/chat`)
+let userList = document.querySelector(".userlist");
+let nownick = "";
+
+function makeMessage(action, payload) {
+    let msg = {};
+    if (action === "new_message") {
+        msg = {"action": action, "message" : payload};
+    } else if (action === "nick") {
+        msg = {"action": "set_nickname", "nick" : payload};
+    } else if (action === "new_connect") {
+        msg = {"action": "user_list"};
+    } else {
+        msg = {"action": action};
+    }
+    return JSON.stringify(msg);
+}
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server O");
+});
+
+socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    getjson = JSON.parse(message.data)
+
+    switch (getjson['action']) {
+        case "join":
+            li.innerText = `${getjson['user']} is joined! `;
+            const user = document.createElement("li");
+            user.innerText = `${getjson['user']}`;
+            userList.append(user);
+            break;
+        case 'new_message':
+            if (nownick === getjson['user']) {
+                li.style.color = 'blue';
+            } 
+            li.innerText = `${getjson['user']} : ${getjson['message']}`;
+            break;
+        case 'left':
+            li.innerText = `${getjson['user']} left this room.`;
+            socket.send(makeMessage("user_list", ""));
+            break;
+        case 'change_nick':
+            li.innerText = `${getjson['from_user']} is changed nickname to ${getjson['to_user']}`;
+            socket.send(makeMessage("user_list", ""));
+            break;
+        case 'connecting':
+            socket.send(makeMessage("new_connect", ""));
+            nownick = getjson['user'];
+            return;
+        case 'user_list':
+            userList.innerHTML = ""
+            for (nick in getjson['users']){
+                const li = document.createElement("li");
+                li.innerText = getjson['users'][nick];
+                userList.append(li);
+            } 
+            return;
+        case 'set_nick':
+            if(getjson['success']) {
+                socket.send(makeMessage("user_list", ""));
+            } else {
+                alert("duplicate nickname is not allowed!");
+            }
+            return;
+        default:
+            console.log("unknown action")
+    }
+    messageList.append(li);
+    chatbox.scrollTop = chatbox.scrollHeight;
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected to Server X");
+});
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nick", input.value));
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
---
+++ server.py
@@ -0,0 +1,115 @@
+from aiohttp import web
+from aiohttp.web_request import Request
+from aiohttp.http_websocket import WSCloseCode, WSMessage
+from pathlib import Path
+from collections import defaultdict
+import random
+from typing import Dict, Tuple, Union
+import jinja2
+import aiohttp_jinja2
+
+here = Path(__file__).resolve().parent
+
+async def index_handler(request: Request) -> web.Response:
+    context = {}
+    response = aiohttp_jinja2.render_template("index.html", request, context=context)
+    
+    return response
+
+async def broadcast(app: web.Application, message: dict) -> None:
+    for x in app['websockets'].items():
+        print(x)
+    for ws in app['websockets'].items():
+        await ws[1].send_json(message)
+
+async def change_nick(
+    app: web.Application, new_nick: str, old_nick: str
+) -> Tuple[Dict[str, Union[str, bool]], bool]:
+
+    if new_nick in app['websockets'].keys():
+        return (
+            {'action': 'set_nick', 'success': False, 'message': 'Name already in use.'},
+            False,
+        )
+    else:
+        app['websockets'][new_nick] = app['websockets'].pop(old_nick)
+        return {'action': 'set_nick', 'success': True, 'message': ''}, True
+
+async def ws_chat(request: Request) -> web.WebSocketResponse:
+    current_websocket = web.WebSocketResponse(autoping=True, heartbeat=60) 
+    ready = current_websocket.can_prepare(request=request)
+    if not ready:
+        await current_websocket.close(code=WSCloseCode.PROTOCOL_ERROR)
+    await current_websocket.prepare(request)
+    user = f'User{random.randint(0, 999999)}'
+
+    await current_websocket.send_json({'action': 'connecting', 'user': user})
+
+    if request.app['websockets'].get(user):
+        await current_websocket.close(code=WSCloseCode.TRY_AGAIN_LATER, message=b'Username already in use')
+        return current_websocket
+    else:
+        request.app['websockets'][user] = current_websocket
+        for ws in request.app['websockets'].values():
+            await ws.send_json({'action': 'join', 'user': user})
+    try:
+        async for message in current_websocket:  
+            if isinstance(message, WSMessage):
+                if message.type == web.WSMsgType.text:  #
+                    message_json = message.json()
+                    action = message_json.get('action')
+                    if action not in ['set_nickname', 'new_message', 'user_list']:
+                        await current_websocket.send_json(
+                            {'action': action, 'success': False, 'message': 'Not allowed.'}
+                        )
+
+                    if action == 'set_nickname':
+                        return_body, success = await change_nick(
+                            app=request.app, new_nick=message_json.get('nick'), old_nick=user
+                        )
+                        if not success:
+                            await current_websocket.send_json(return_body)
+                        else:
+                            await current_websocket.send_json(return_body)
+                            await broadcast(
+                                app=request.app,
+                                message={
+                                    'action': 'change_nick',
+                                    'from_user': user,
+                                    'to_user': message_json.get('nick'),
+                                },
+                            )  
+                            user = message_json.get('nick')
+
+                    elif action == 'user_list':
+                        user_list =  {'action': 'user_list', 'success': True, 'users': list(app['websockets'].keys())}
+                        await current_websocket.send_json(user_list)
+
+                    elif action == 'new_message':
+                        await broadcast(
+                            app=request.app,
+                            message={'action': 'new_message', 'message': message_json.get('message'), 'user': user},
+                        )
+    finally:
+        request.app['websockets'].pop(user)
+
+    await broadcast(
+        app=request.app, message={'action': 'left', 'user': user}
+    )
+
+    return current_websocket
+
+app = web.Application()
+aiohttp_jinja2.setup(
+    app, loader=jinja2.FileSystemLoader(here)
+)
+app['websockets'] = defaultdict(dict)
+app.router.add_static('/prefix',str(here)+'/public', show_index=True)
+app.router.add_get('/', index_handler)
+app.router.add_get('/chat', handler=ws_chat)
+
+
+if __name__ == '__main__':
+    web.run_app(app)
+
+
